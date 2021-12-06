0aeb24f3396dae0ae8e68136dba3cab79357f5e3
Maggie McCausland <maggiemccausland@hotmail.com>
2021-12-06 12:08:36 -0700

    made changes to quotes content and styling

--- src/components/QuotesCard.js
+++ src/components/QuotesCard.js
@@ -12,36 +12,28 @@ const QuotesCard = (props) => {
         maxWidth: "450px",
         m: 2,
         padding: "20px",
+        backgroundColor: "black",
+        color: "white",
+        border: "2px solid white",
         borderRadius: "5px",
-        backgroundColor: "white",
         fontWeight: "300",
         height: "auto",
-        // "&:hover": {
-        //   backgroundColor: "green",
-        //   color: "white",
-        // }
+        "&:hover": {
+          backgroundColor: "white",
+          color: "black",
+        }
       }}
     >
-      <Box
-        sx={{
-          height: "auto",
-          padding: 0,
-          margin: "auto auto",
-        }}
-      >
         <Box sx={{
-          padding: "auto 5%", margin: "0", height: "200px",
+          padding: "1% 3%", margin: "0", height: "150px",
           display: "flex",
           justifyContent: "center",
           alignContent: "center",
           flexDirection: "column",
         }}>
-          {/* <Typography variant="body1" sx={{ textAlign: "left", margin: 0 }}>#{quote.quote_id}</Typography> */}
           <Typography variant="body1" sx={{ fontSize: "20px", lineHeight: "normal", textAlign: "center", margin: "15px 0", fontWeight: "bold" }}>"{quote.quote}"</Typography>
-          <Typography variant="body1" sx={{ textAlign: "right", margin: 0 }}>- {quote.author}</Typography>
+          <Typography variant="body1" sx={{ fontSize: "18px", textAlign: "right", margin: 0 }}>- {quote.author}</Typography>
         </Box>
-
-      </Box>
     </Box>
   );
 };
